01e8321b813044f50882a56574652723cd51f94f
Kseniya Baluk <kseniya.baluk@broadsign.com>
2022-05-20 11:54:40 -0400

    Add nodemon

--- src/app.ts
+++ src/app.ts
@@ -9,32 +9,32 @@ import { TYPES } from './types';
 // must be marked as injectable
 @injectable()
 export class App {
-	app: Express;
-	server: Server;
-	port: number;
+    app: Express;
+    server: Server;
+    port: number;
 
-	// injecting dependencies
-	constructor(
-		@inject(TYPES.ILogger) private logger: ILogger,
-		@inject(TYPES.IUsersController) private userController: UsersController,
-		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
-	) {
-		this.app = express();
-		this.port = 7979;
-	}
+    // injecting dependencies
+    constructor(
+        @inject(TYPES.ILogger) private logger: ILogger,
+        @inject(TYPES.IUsersController) private userController: UsersController,
+        @inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+    ) {
+        this.app = express();
+        this.port = 7979;
+    }
 
-	useRoutes(): void {
-		this.app.use('/users', this.userController.router);
-	}
+    useRoutes(): void {
+        this.app.use('/users', this.userController.router);
+    }
 
-	useExceptionFilters(): void {
-		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
-	}
+    useExceptionFilters(): void {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
 
-	public async init(): Promise<void> {
-		this.useRoutes();
-		this.useExceptionFilters();
-		this.server = this.app.listen(this.port);
-		this.logger.log(`Listening on http://localhost:${this.port}`);
-	}
+    public async init(): Promise<void> {
+        this.useRoutes();
+        this.useExceptionFilters();
+        this.server = this.app.listen(this.port);
+        this.logger.log(`Listening on http://localhost:${this.port}`);
+    }
 }
